82f3aaff1efa91d096a25d938f3c4b5663afeb18
Eghinner <eghinner@gmail.com>
2022-01-21 22:20:12 -0800

    .

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -11,8 +11,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
@@ -120,4 +120,6 @@ class PostsController extends AbstractController
         'posts' => $posts
        ]);
     }
+
+
 }
